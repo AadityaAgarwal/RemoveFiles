f94dbbe82a8501d3ee8d9128a76c445f4bca8dbe
AadityaAgarwal <73215428+AadityaAgarwal@users.noreply.github.com>
2021-06-14 19:23:41 +0530

    Update remove.py

--- remove.py
+++ remove.py
@@ -1,22 +1,73 @@
-import time,shutil,os
-path=input("Enter the path: ")
-days=input("Enter the number of days: ")
-timeCalc=time.time()
-check=os.path.exists(path)
-listFile=os.listdir(path)
 
-for file in listFile:
-    name,ext=os.path.splitext(file)
+import os
+import shutil
+import time
 
-    if(check==True) :
-      walk= os.walk(path)
-      os.path.join(path)
-      ctime=os.stat(path).st_ctime
-      print(ctime)
+def main():
+	path = "e:/sameena notes"
+	days = 30
+	seconds = time.time() - (days * 24 * 60 * 60)
 
-      if(ext==' '):
-          shutil.rmtree()
-      else:
-          os.remove(file)    
-    else:
-         print("Path not found!")
+	if os.path.exists(path):
+
+		for root_folder, folders, files in os.walk(path):
+
+			if seconds >= get_file_or_folder_age(root_folder):
+
+				remove_folder(root_folder)
+				break
+
+			else:
+
+				for folder in folders:
+
+					folder_path = os.path.join(root_folder, folder)
+
+					if seconds >= get_file_or_folder_age(folder_path):
+
+						remove_folder(folder_path)
+						 
+				for file in files:
+
+					file_path = os.path.join(root_folder, file)
+
+					if seconds >= get_file_or_folder_age(file_path):
+
+						remove_file(file_path)
+		else:
+
+			if seconds >= get_file_or_folder_age(path):
+
+				remove_file(path)
+				 
+	else:
+
+		print(f'"{path}" is not found')
+		 
+def remove_folder(path):
+
+	if not shutil.rmtree(path):
+
+		print(f"{path} is removed successfully")
+
+	else:
+
+		print(f"Unable to delete the "+path)
+
+def remove_file(path):
+
+	if not os.remove(path):
+
+		print(f"{path} is removed successfully")
+
+	else:
+
+		print("Unable to delete the "+path)
+
+def get_file_or_folder_age(path):
+
+	ctime = os.stat(path).st_ctime
+	 
+	return ctime
+if __name__ == '__main__':
+	main()
